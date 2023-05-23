9ca949e91db70fe9447566d84776d0dddec9819d
VictorMagaCBT <126924738+VictorMagaCBT@users.noreply.github.com>
2023-05-23 13:38:54 +0000

    startfunction

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+/*window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  /*let nipes = ["<i class="fa-solid fa-heart">", "<i class="fa-solid fa-spade">", "<i class="fa-solid fa-diamond">", "<i class="fa-solid fa-club">"];
+  let faces = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let nipe = nipes[Math.floor(Math.random() * 4)];
+  let face = faces[Math.floor(Math.random() * faces.length)];
+  document.write(nipe + face);
+};*/
+document.getElementById;
+let card = (nipe, face);
